7e80d44a708c771d77bc9876104b0a28d3d03de2
Pankaj Chauhan <51152024+Pnkjo7@users.noreply.github.com>
2022-03-22 16:10:15 +0530

    22/03/2022

--- Main.java
+++ Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 		Parent root = FXMLLoader.load(getClass().getResource("Form1.fxml"));
 		Scene scene = new Scene(root,850,650);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setTitle("Online Requisition Raising");
 		primaryStage.setResizable(false);
 		
@@ -23,6 +24,7 @@ public class Main extends Application {
 		
 	}
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
