c088243c5efc7f471f4888aa2244035141e15217
Pankaj Chauhan <51152024+Pnkjo7@users.noreply.github.com>
2022-04-01 17:27:40 +0530

    april change

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 package application;
+	
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -11,18 +13,23 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage)   {
 		try {
-		Parent root = FXMLLoader.load(getClass().getResource("Form1.fxml"));
-		Scene scene = new Scene(root,850,650);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setTitle("Online Requisition Raising");
-		primaryStage.setResizable(false);
+			Parent root = FXMLLoader.load(getClass().getResource("Form1.fxml"));
+			Scene scene = new Scene(root,850,650);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Investigation Raising & Sample Collection");
+			primaryStage.setResizable(false);
 		
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}catch(Exception e) {
+			primaryStage.setScene(scene);
+			Image I = new Image(getClass().getResourceAsStream("laboratory-test.png"));
+			primaryStage.getIcons().add(I);
+			primaryStage.show();
+			
+			
+		}catch(Exception e) {
 		e.printStackTrace();
 		
-	}
+		
+		}
 	}
 	
 	
